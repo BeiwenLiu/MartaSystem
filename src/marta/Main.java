2adec5cc24627507bb9bff63c0fc22c332865bb8
Beiwen Liu <beiwenl@gmail.com>
2018-07-09 02:37:35 -0400

    add source file

---
+++ src/marta/Main.java
@@ -0,0 +1,11 @@
+package marta;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Mass Transit Simulation System Starting...");
+
+        SimDriver commandInterpreter = new SimDriver();
+        commandInterpreter.runInterpreter();
+    }
+}
